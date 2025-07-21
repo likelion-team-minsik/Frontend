d76a72fa64c0d777dfe937fcc91f2ec3cc720d5d
twolim <happygrove8@gmail.com>
2025-07-21 13:45:43 +0900

    [Feat] 프로필 조회, 수정, 로그아웃 기능 연동

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -9,6 +9,22 @@ const Menu = ({ isOpen, onClose }) => {
     onClose();
   };
 
+  const getTamcleTalkPath = () => {
+    const token = localStorage.getItem("authToken");
+    const hasAccountHistory = localStorage.getItem("hasAccountHistory"); // 회원가입/로그인 이력 플래그
+
+    if (token) {
+      //1. 토큰이 있으면 로그인 버전 탐클톡2 페이지
+      return "/TamcleTalk2";
+    } else if (hasAccountHistory === "true") {
+      //2. 토큰은 없지만 계정 이력이 있으면 탐클톡3 페이지 (로그아웃 상태)
+      return "/TamcleTalk3";
+    } else {
+      //3. 토큰도 없고 계정 이력도 없으면 비회원 탐클톡1 페이지 (완전 비회원 상태)
+      return "/TamcleTalk1";
+    }
+  };
+
   return (
     <M.MenuBox isOpen={isOpen}>
       <img
@@ -38,7 +54,7 @@ const Menu = ({ isOpen, onClose }) => {
         장비 구경하기
       </div>
 
-      <div id="menu5" onClick={() => goPage("/TamcleTalk1")}>
+      <div id="menu5" onClick={() => goPage(getTamcleTalkPath())}>
         {/*1=비회원 2=로그인 완료 3=로그아웃 */}
         탐클톡
       </div>
